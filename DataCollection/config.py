6ddd58a3bf757f8661f19b492ab9aa8f4c9239c8
Telescope-U <53029691+Telescope-U@users.noreply.github.com>
2022-06-30 12:41:06 +0100

    add infos_collection module

--- DataCollection/config.py
+++ DataCollection/config.py
@@ -3,7 +3,7 @@ TRANSCRIPT_FOLDER = "../Dataset/Transcripts/"
 INFO_PATH = "../Dataset/videos.csv"
 
 TRANSCRIPT_HEAD = ('vid','start', "end", "text")
-INFO_HEAD = ('vid', 'title','time','duration', 'views','link')
+INFO_HEAD = ('vid', 'title','tag','time','duration', 'views','link')
 COMMENT_HEAD = ('vid', 'uid', 'content', 'votes')
 
 KEYWORDS = ('News', 'Sport','Food', 'Game', 'Fashion', 'Entertainment', 'Tech', 'Education', 'Motivation', 'Vlog', 'Art', 'Music')
